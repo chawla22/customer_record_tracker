1a8d7d9ef6b6bed1d2b0ea823a9f1d4ed5e7b0ba
chawla22 <chawla22@usf.edu>
2025-06-01 11:36:02 +0530

    Create 3 Customer Records

---
+++ cc_2.js
@@ -0,0 +1,32 @@
+// Step 2: Create Customer Records
+// Create an array called customers.
+// Inside this array, create at least three customer objects.
+// Each customer object should have the following properties:
+// name (string)
+// email (string)
+// purchases (array of strings)
+
+customers = [
+    {
+        name: "somya chawla",
+        email: "chawla22@usf.edu",
+        purchases: ["Laptop", "Smartphone"]
+    },
+    {
+        name: "john doe",
+        email: "doe@info.com",
+        purchases: ["Tablet", "Smartwatch"]
+    },
+
+    {
+        name: "jane smith",
+        email: "smith@info.com",
+        purchases: ["Headphones", "Smart Speaker"]
+    }
+];
+
+console.log("creating an array of  3 customers ...!!!");
+console.log(customers);
+
+
+
