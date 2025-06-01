dd45e3554c4b042193afb05af9b5d479967c309e
chawla22 <chawla22@usf.edu>
2025-06-01 11:53:29 +0530

    Add a new customer to the customers array using .push()

--- cc_2.js
+++ cc_2.js
@@ -42,9 +42,5 @@ console.log("after adding a new customer...!!!");
 console.log(customers);
 
 
-// Remove the first customer using .shift().
-console.log("after removing  the first customer...!!!");
-customers.shift();
-console.log(customers);
 
 
